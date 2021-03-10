5a3a432982bbf812f021bd2ede2c46d50ea4529b
eduardojst10 <eduardojst99@gmail.com>
2021-03-10 14:18:24 +0000

    Alteraçoes em pessoas

--- guiao1/pessoas/pessoas.c
+++ guiao1/pessoas/pessoas.c
@@ -11,19 +11,56 @@ int inserePessoa(char* nome, int idade,int fd){
     if(fd == -1) {
                 puts("ERROR - Couldn't open or create registry file.");
                 return 1;
-            }
-
+    }
+    
     Pessoa pessoa;
     pessoa.idade = idade;
     strcpy(pessoa.nome,nome);
     
-    if(write(fd,&pessoa,sizeof(struct pessoa)) < 1){
+    if(write(fd,&pessoa,sizeof(struct pessoa)) < 1){ //buffer é um apontador 
         puts("Error - Nada escrito"); //stdout
         close(fd);
-        return -1;
+        return 1;
 
     }
-
+    /*
+    struct stat st;
+    fstat(fd, &st);
+    off_t filesize = st.st_size;
+    int pos = (int)filesize / sizeof(struct pessoa);
+    */
+    puts("Sucesso ");
+    close(fd);
     return 0;
 
+}
+
+int altera_Idade(char* nome,int idade,int fd){
+    Pessoa p;
+    int bytes_read;
+    int res;
+
+    while((bytes_read) = read(fd,&p,sizeof(struct pessoa))){
+
+        if(strcmp(p.nome,nome)==0){
+                    res = lseek(fd,-sizeof(Pessoa),SEEK_CUR);
+                    if(res<0){
+                        perror("Error lseek");
+                        return -1;
+                    }
+
+                    p.idade = idade;
+                    res = write(fd,&p,sizeof(Pessoa));
+                    if(res < 0){
+                        perror("Error write");
+                        return -1;
+                    }
+        }
+    }
+    
+        
+    };
+
+    
+    return 0;
 }
--- guiao1/pessoas/pessoas.h
+++ guiao1/pessoas/pessoas.h
@@ -4,13 +4,10 @@
 typedef struct pessoa{
     char nome[200];
     int idade;
-
-    
-
 }Pessoa;
 
 
 int inserePessoa(char* nome, int idade,int fd);
-
+int altera_Idade(char* nome,int idade,int fd);
 
 #endif
